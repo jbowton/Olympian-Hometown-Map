07c2f4945b379f72f5e071c00f7891c2c008a9b9
jbowton <jbowton@cs.washington.edu>
2024-11-20 00:54:13 -0800

    Merged datasets

--- Olympic_Athletes.py
+++ Olympic_Athletes.py
@@ -6,6 +6,8 @@ url = "https://www.teamusa.com/athletes"
 response = requests.get(url)
 soup = BeautifulSoup(response.content, "html.parser")
 
+cities_df = pd.read_csv("uscities.csv")
+
 athletes = []
 for card in soup.find_all("tr", class_="css-0"):
     name = card.find("p", class_= "chakra-text css-mpfnrl").text.strip()
@@ -13,19 +15,27 @@ for card in soup.find_all("tr", class_="css-0"):
     hometown = p_tags[len(p_tags) - 1].text.strip()
     athletes.append({"Name": name, "Hometown": hometown})
 
-cities_df = pd.read_csv("uscities.csv")
-
 # List of cities from the dataset
 cities_list = cities_df['city'].str.lower().tolist()
 # Convert to set for faster look up
 cities_set = set(cities_list)
 
+#states_list = cities_df['state_id'].st
+
 # Iterate over athletes' hometown and check if the hometown is valid
 for athlete in athletes:
-    hometown = athlete.get("Hometown", "").lower()
+    #Remove state code for check
+    hometown = athlete.get("Hometown", "").lower()[:-4]
     if hometown not in cities_set:
         athlete["Hometown"] = ""
 
 # Save to CSV
-df = pd.DataFrame(athletes)
-df.to_csv("team_usa_athletes.csv", index=False)
+athletes_df = pd.DataFrame(athletes)
+#Merge athlete to city data
+athletes_df[['City', 'State']] = athletes_df['Hometown'].str.split(',', expand=True)
+athletes_df['State'] = athletes_df['State'].str.strip()  # Remove extra spaces
+merged_df = athletes_df.merge(cities_df, how='left', left_on=['City', 'State'], right_on=['city', 'state_id'])
+
+# Save the updated dataframe to CSV
+merged_df.to_csv("team_usa_athletes_with_coords.csv", index=False)
+#df.to_csv("team_usa_athletes.csv", index=False)
