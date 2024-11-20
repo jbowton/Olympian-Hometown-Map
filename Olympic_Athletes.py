d171d99d1bc53b8d6206f23598b7145d9bda735a
jbowton <jbowton@cs.washington.edu>
2024-11-20 00:23:25 -0800

    Added check for valid Hometown

--- Olympic_Athletes.py
+++ Olympic_Athletes.py
@@ -10,13 +10,21 @@ athletes = []
 for card in soup.find_all("tr", class_="css-0"):
     name = card.find("p", class_= "chakra-text css-mpfnrl").text.strip()
     p_tags = card.find_all("p", class_="chakra-text css-1qjehc2")
-    #if len(p_tags) == 4:
-      #hometown = p_tags[3].text.strip()  # Adjust as needed
-    #elif len(p_tags) == 3:
-       #hometown = p_tags[2].text.strip()
     hometown = p_tags[len(p_tags) - 1].text.strip()
     athletes.append({"Name": name, "Hometown": hometown})
-    print(p_tags)
+
+cities_df = pd.read_csv("uscities.csv")
+
+# List of cities from the dataset
+cities_list = cities_df['city'].str.lower().tolist()
+# Convert to set for faster look up
+cities_set = set(cities_list)
+
+# Iterate over athletes' hometown and check if the hometown is valid
+for athlete in athletes:
+    hometown = athlete.get("Hometown", "").lower()
+    if hometown not in cities_set:
+        athlete["Hometown"] = ""
 
 # Save to CSV
 df = pd.DataFrame(athletes)
