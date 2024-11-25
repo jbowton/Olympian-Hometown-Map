3b2c8fa4d09e51dc442a7a0592f37438df4ef8a3
jbowton <jbowton@cs.washington.edu>
2024-11-25 10:12:17 -0800

    Added 2020 athletes

--- mergeData.py
+++ mergeData.py
@@ -1,17 +1,28 @@
 import pandas as pd
 
 # Load the Olympic athletes data from Excel
-athletes_df = pd.read_excel('Olympic_roster2024.xlsx', engine='openpyxl')
+athletes_df = pd.read_excel('2020_US_Olympic_TeamAthletes_by_state.xlsx', engine='openpyxl')
+
+if 'Name' in athletes_df.columns:
+    # Split the Name column into First Name and Last Name
+    athletes_df[['First Name', 'Last Name']] = athletes_df['Name'].str.split(' ', n=1, expand=True)
+    # Drop the original Name column
+    athletes_df = athletes_df.drop('Name', axis=1)
 
 # Load the city coordinates data from CSV
 cities_df = pd.read_csv('uscities.csv')
 
-# Standardize column names for merging (adjust these to match your data)
-athletes_df.rename(columns={'Hometown City': 'Hometown', 'Hometown State': 'State'}, inplace=True)
+if 'Hometown' in athletes_df.columns:
+    athletes_df[['Hometown', 'State']] = athletes_df['Hometown'].str.split(',', n=1, expand=True)
+    athletes_df['Hometown'] = athletes_df['Hometown'].str.strip().str.title()
+    athletes_df['State'] = athletes_df['State'].str.strip()
 
-# Ensure proper casing and remove extra spaces
-athletes_df['Hometown'] = athletes_df['Hometown'].str.strip().str.title()
-cities_df['city'] = cities_df['city'].str.strip().str.title()
+    #athletes_df = athletes_df.drop('Hometown', axis=1)
+
+
+# Standardize column names for merging (adjust these to match your data)
+#athletes_df.rename(columns={'Hometown City': 'Hometown', 'Hometown State': 'State'}, inplace=True)
+
 
 # Merge the datasets on the 'City' column
 merged_df = pd.merge(athletes_df, cities_df, left_on=['Hometown', 'State'], right_on=['city', 'state_name'], how='left')
@@ -19,4 +30,4 @@ merged_df = pd.merge(athletes_df, cities_df, left_on=['Hometown', 'State'], righ
 filteredMerged_df = merged_df.filter(items=["First Name", "Last Name", "Sport", "Hometown", "State", "Gender", "state_id", "lat", "lng", "population"])
 
 # Save the combined dataset to a new Excel file
-filteredMerged_df.to_csv('combined_athletes_data.csv', index=False)
+filteredMerged_df.to_csv('2020combined_athletes.csv', index=False)
